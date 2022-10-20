6bab79067c4e3c0e303393dcd653c54d2938fd68
Endre Moen <emoen@users.noreply.github.com>
2022-10-20 14:08:49 +0200

    interview question solution with runtime O(n^2) and together with optimal solution O(n)

---
+++ src/no/interview/question/NonRepeatingCharacterFinder.java
@@ -0,0 +1,102 @@
+package no.interview.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
+public class NonRepeatingCharacterFinder {
+
+	/**
+	 * Implementation with O(k) running time 
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static char findFirstNonRepeatingCharacterBetter(String input) {
+		
+		HashMap<Character, Integer> counter = new HashMap<Character, Integer>();
+		
+		for(int i=0; i < input.length(); i++) {
+			Character next = input.charAt(i);
+			counter.put(next, counter.getOrDefault(next, 0)+1);
+		}
+		
+		for(int i=0; i < input.length(); i++) {
+			if ( counter.get(input.charAt(i)) == 1) {
+				return input.charAt(i);
+			}
+		}
+		return 0;
+	}
+	
+
+	/**
+	 * Implementation with O(n^2) running time and overly complex
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static char findFirstNonRepeatingCharacter(String input) {
+		int index = 0;
+		List<String> charactersFound = new ArrayList<String>();
+		while (index < input.length()) {
+			char pointing = input.charAt(index);
+			int search = index + 1;
+
+			while (search < input.length() && charactersFound.contains(pointing + "")) {
+				index++;
+				pointing = input.charAt(index);
+				search = index + 1;
+			}
+
+			while (search < input.length() && input.charAt(search) != pointing) {
+				search++;
+			}
+			if (search < input.length()) {
+				charactersFound.add(pointing + "");
+//				System.out.println("adding character pointing:" + charactersFound.toString());
+			} else if (!charactersFound.contains(pointing + "")) {
+//				System.out.println("char pointing:" + pointing + " charfound" + charactersFound.toString());
+				return pointing;
+			}
+			index++;
+		}
+		return 0;
+	}
+	
+	public static void main( String[] args ) {
+		
+		String example = "aaabbeccdfgfg";
+		String example2 = "abbeccdfgfga";
+		String example3 = "abbccdfgfga";
+		String empty = "";
+		
+		char expectedE = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacterBetter(example);
+		System.out.println(expectedE);
+		assert expectedE=='e';
+		
+		expectedE = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacterBetter(example2);
+		assert expectedE=='e';
+				
+		char expectedZero = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacterBetter(example3);
+		assert expectedZero == '0';
+		
+		expectedZero = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacterBetter( empty );
+		assert expectedZero == '0';
+		
+		// old implementation tests
+		
+		expectedE = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacter(example);
+		assert expectedE=='e';
+		
+		expectedE = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacter(example2);
+		assert expectedE=='e';
+				
+		expectedZero = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacter(example3);
+		assert expectedZero == '0';
+		
+		expectedZero = NonRepeatingCharacterFinder.findFirstNonRepeatingCharacter( empty );
+		assert expectedZero == '0';
+	}
+	
+}
